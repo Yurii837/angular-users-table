7f6950994ff5915d1f136a984de5debf96d3de7a
yurii837 <gorogotskiy@gmail.com>
2022-08-20 17:18:09 +0300

    create app add DevExtreme lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-users-table';
+
+
+  helloWorld(e: any) {
+    console.log(e)
+  }
+
+  // employees: Employee[];
+
+  // constructor(private service: Service) {
+  //   this.employees = service.getEmployees();
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DxButtonModule } from 'devextreme-angular';
+// import { DxDataGridModule, DxTemplateModule } from 'devextreme-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DxButtonModule,
+    // DxDataGridModule,
+    // DxTemplateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
